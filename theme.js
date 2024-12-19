10c96c6713220159d79f61c688387fbd221f932d
sjs <worodk38@gmail.com>
2024-12-19 09:49:32 +0900

    add dark mode

---
+++ theme.js
@@ -0,0 +1,9 @@
+function setLightMode() {
+    document.querySelector('body').style.background = 'white';
+    document.querySelector('body').style.color = 'black';
+}
+
+function setDarkMode() {
+    document.querySelector('body').style.background = 'black';
+    document.querySelector('body').style.color = 'white';
+}
