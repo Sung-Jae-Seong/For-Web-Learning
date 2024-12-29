294d0aad10ce05623b24ea253a977e6c03e9bbbc
sjs <wordok@gmail.com>
2024-12-29 13:28:05 +0900

    add Object/jQuery

--- theme.js
+++ theme.js
@@ -1,27 +1,50 @@
 // === : equal
 // &lt; : less than on html '<'
 
+var Body = {
+    setColor:function (color){
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+    },
+    setBackground:function (color){
+        $('body').css('background', color);
+    }
+}
 
+var Links = {
+    setColor:function(color){
+    //     alist = document.querySelectorAll('a');
+    //     for (let i = 0; i < alist.length; i++) { //'let' becomes local variable of for statement, var becomes variable
+    //         alist[i].style.color=color;
+    //     }
+        $('a').css('color', color);
+    }
+}
+
+function setColor(target, color){
+    for (let i = 0; i < target.length; i++) { //'let' becomes local variable of for statement, var becomes variable
+        target[i].style.color=color;
+    }
+}
 function setDarkLightToggle() {
     const button = document.querySelector('#dark_light_toggle');
     const body_style = document.querySelector('body').style;
     const alist = document.querySelectorAll('a');
 
     if(button.value==="dark"){
-        body_style.background = 'black';
-        body_style.color = 'white';
+        //body_style.background = 'black';
+        //setColor(body_style.color, 'white');
         button.value = "light";
-        for (let i = 0; i < alist.length; i++) { //let becomes local variable of for statement, var becomes variable
-            alist[i].style.color='powderblue';
-        }
+        //setColor(alist, 'powderblue');
+        Body.setBackground('black');
+        Body.setColor('white');
+        Links.setColor('powderblue');
     }
     else{
         body_style.background = 'white';
-        body_style.color = 'black';
+        setColor(body_style.color, 'black');
         button.value = "dark";
-        for (let i = 0; i < alist.length; i++) { //let becomes local variable of for statement, var becomes variable
-            alist[i].style.color='blue';
-        }
+        setColor(alist, 'blue');
     }
 
 
