7087c76280c837b5a9bb8c7a56eef8d676211113
sjs <wordok@gmail.com>
2025-01-02 20:12:14 +0900

    fix the bug

--- theme.js
+++ theme.js
@@ -1,61 +1,39 @@
-// === : equal
-// &lt; : less than on html '<'
-
 var Body = {
-    setColor:function (color){
-        // document.querySelector('body').style.color = color;
+    setColor: function(color) {
         $('body').css('color', color);
     },
-    setBackground:function (color){
+    setBackground: function(color) {
         $('body').css('background', color);
     }
-}
+};
 
 var Links = {
-    setColor:function(color){
-    //     alist = document.querySelectorAll('a');
-    //     for (let i = 0; i < alist.length; i++) { //'let' becomes local variable of for statement, var becomes variable
-    //         alist[i].style.color=color;
-    //     }
+    setColor: function(color) {
         $('a').css('color', color);
     }
-}
+};
 
-function setColor(target, color){
-    for (let i = 0; i < target.length; i++) { //'let' becomes local variable of for statement, var becomes variable
-        target[i].style.color=color;
-    }
-}
 function setDarkLightToggle() {
     const button = document.querySelector('#dark_light_toggle');
-    const body_style = document.querySelector('body').style;
-    const alist = document.querySelectorAll('a');
+    const currentMode = button.getAttribute('data-mode');
 
-    if(button.value==="dark"){
-        //body_style.background = 'black';
-        //setColor(body_style.color, 'white');
-        button.value = "light";
-        //setColor(alist, 'powderblue');
+    if (currentMode === "dark") {
+        button.setAttribute('data-mode', "light");
         Body.setBackground('black');
         Body.setColor('white');
         Links.setColor('powderblue');
+    } else {
+        button.setAttribute('data-mode', "dark");
+        Body.setBackground('white');
+        Body.setColor('black');
+        Links.setColor('blue');
     }
-    else{
-        body_style.background = 'white';
-        setColor(body_style.color, 'black');
-        button.value = "dark";
-        setColor(alist, 'blue');
-    }
-
-
 }
 
 function setLightMode() {
-    document.querySelector('body').style.background = 'white';
-    document.querySelector('body').style.color = 'black';
+    $('body').css('background', 'white').css('color', 'black');
 }
 
 function setDarkMode() {
-    document.querySelector('body').style.background = 'black';
-    document.querySelector('body').style.color = 'white';
+    $('body').css('background', 'black').css('color', 'white');
 }
